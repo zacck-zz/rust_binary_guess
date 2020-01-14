25dfe07e593abe1bcc81cf3274008ded7df7dceb
Zacck Osiemo <coderv63@gmail.com>
2020-01-14 08:08:40 +0200

    Implement guess matching logic
    - Convert the input string into a u32 integer using shadowing
    - Add pattern matching to check if the input matches  the secret number
    - Implement loop to keep asking for guesses if user is wrong and break
    out if right

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
+//import the io library
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
+// entry point into the program
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        //variable to hold the guess
+        //note this is a mutable variable as provided by the mut keyword
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);
+        //read input from std in, into the guess variable
+        io::stdin()
+            .read_line(&mut guess)
+            //failure handler with the Result type this line will be called if the above fails
+            .expect("Failed to read line");
+
+        //convert guess into a real number using shadowing to reuse the previous variable
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("You guessed: {}", guess);
+
+        //compare the guess to secret number
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
