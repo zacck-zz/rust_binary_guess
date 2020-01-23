960653437f83675d130f68d2679d53dff08fd786
Zacck Osiemo <coderv63@gmail.com>
2020-01-23 07:07:24 +0200

    Define struct and create instances of it

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,44 @@
 // entry point into the program
-fn main() {}
+fn main() {
+    //create an instance of a struct
+    let mut user1 = User {
+        email: String::from("me@me.com"),
+        username: String::from("superbike_z"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    // access instance variables
+    println!(
+        "Uname, email, active, sign_count: {} {} {} {}",
+        user1.username, user1.email, user1.active, user1.sign_in_count
+    );
+
+    // update mutable instance variables using dot notation
+    user1.sign_in_count = 2;
+
+    println!("Updated sign in count: {}", user1.sign_in_count);
+
+    let mut user2 = build_user(String::from("her@her.com"), String::from("dino_c"));
+
+    println!("User2 name is: {}", user2.username);
+
+    user2.sign_in_count = 2;
+}
+
+// sample struct for a user type
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
