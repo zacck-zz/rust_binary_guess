d34587c2bdd49587afa7a6a90f65d7e011247645
Zacck Osiemo <coderv63@gmail.com>
2020-01-27 10:31:16 +0200

    Implement struct reactangle and calculate the area using a struct method

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,24 @@ fn main() {
         tuple_area(rect1)
     );
 
+    let rect2 = Rectangle {
+        width: width1,
+        height: height1,
+    };
+
+    println!(
+        "The area when using a rectangle struct is {} squared pixels.",
+        rect_area(&rect2)
+    );
+
+    // debug a struct
+    println!("Here is our rectangle \n {:#?}", rect2);
+
+    println!(
+        "The area when using a struct method is {} squared pixels.",
+        rect2.area()
+    );
+
     //create an instance of a struct
     let mut user1 = User {
         email: String::from("me@me.com"),
@@ -94,4 +112,20 @@ fn area(width: u32, height: u32) -> u32 {
 
 fn tuple_area(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
+}
+
+fn rect_area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
