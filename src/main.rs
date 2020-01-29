d61f3e58e626efdb9748481410f5ee60f2179fe1
Zacck Osiemo <coderv63@gmail.com>
2020-01-29 06:48:31 +0200

    Build IP address enum and construct it with data

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,20 @@
 // entry point into the program
-fn main() {}
+fn main() {
+    let home = IpAddrKind::V4(127, 0, 0, 1);
+    let loopback = IpAddrKind::V6(String::from("::1"));
+
+    println!(
+        "The home address is: {:?} and the loopback is: {:?}",
+        home, loopback
+    );
+}
+
+//define an IP enum
+#[derive(Debug)]
+enum IpAddrKind {
+    // Add data into enum variants to be more concise
+    // This way each variant can take in as many arguments as it needs
+    // negating the need for another datastructure such as a record or map
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
